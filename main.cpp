47d4e57743d6e961c6fc3b6b41788ed2710d1ee9
JohnEsparagus <johnedesparagused@gmail.com>
2025-06-24 14:07:19 +0100

    added loop for gkeeping window open

--- main.cpp
+++ main.cpp
@@ -7,13 +7,16 @@ int main() {
     return -1;
   }
 
-  SDL_Window *window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, 0);
+  int width = 680;
+  int height = 480;
+
+  SDL_Window *window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
 
   if (!window) {
     std::cout << "Failed to create the window\n";
     return -1;
   } 
-  
+   
   SDL_Surface *window_surface = SDL_GetWindowSurface(window);
 
   if(!window_surface) {
@@ -21,7 +24,33 @@ int main() {
     return -1;
   }
 
+  int mouse_x = 0;
+  int mouse_y = 0;
   SDL_UpdateWindowSurface(window);
-
-  SDL_Delay(5000);
+  bool running = true;
+  while (running)
+  {
+    for (SDL_Event event; SDL_PollEvent(&event);) switch (event.type)
+      {
+      case SDL_WINDOWEVENT:
+        switch (event.window.event)
+        {
+        case SDL_WINDOWEVENT_RESIZED:
+            width = event.window.data1;
+            height = event.window.data2;
+            break;
+        }
+        break;
+      case SDL_QUIT:
+	SDL_Delay(1000);
+        running = false;
+        break;
+      case SDL_MOUSEMOTION:
+        mouse_x = event.motion.x;
+        mouse_y = event.motion.y;
+        break;
+      }
+    if (!running)
+        break;
+  }
 }
